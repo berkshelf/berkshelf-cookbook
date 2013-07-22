62900934bf3531b621ad4b2b9fb32cb2fb013fbe
Jamie Winsor <jamie@vialstudios.com>
2013-07-22 10:19:19 -0700

    version bump 0.2.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jamie@vialstudios.com'
 license          'Apache 2.0'
 description      'Installs/Configures berkshelf'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 
 supports 'ubuntu'
 
